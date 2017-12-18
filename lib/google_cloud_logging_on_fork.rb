549c8a3a41274dcc4d3fbd6bcfabb757c26b5723
Akihiro MATSUMURA <matsumura.aki@gmail.com>
2017-12-18 21:51:11 +0900

    ログ転送されない

--- lib/google_cloud_logging_on_fork.rb
+++ lib/google_cloud_logging_on_fork.rb
@@ -2,10 +2,12 @@ require "google_cloud_logging_on_fork/version"
 
 module GoogleCloudLoggingOnFork
   def self.load(project_id = nil)
-    require 'google/cloud/logging'
-    Google::Cloud::Logging.configure do |config|
-      config.project_id = project_id || rails_config_project_id
-      config.keyfile = rails_config_keyfile
+    if require 'google/cloud/logging'
+      Google::Cloud::Logging.configure do |config|
+        config.project_id = project_id || rails_config_project_id
+        config.keyfile = rails_config_keyfile if rails_config_keyfile
+        config.labels = rails_config_labels if rails_config_labels
+      end
     end
   end
 
@@ -15,6 +17,10 @@ module GoogleCloudLoggingOnFork
 
   def self.rails_config_keyfile
     defined?(::Rails) && ::Rails.application.config.google_cloud_logging_on_fork.keyfile
+  end
+
+  def self.rails_config_labels
+    defined?(::Rails) && ::Rails.application.config.google_cloud_logging_on_fork.labels
   end
 end
 
--- lib/google_cloud_logging_on_fork/middleware.rb
+++ lib/google_cloud_logging_on_fork/middleware.rb
@@ -1,11 +1,12 @@
 module GoogleCloudLoggingOnFork
   class Middleware
-    def initialize app, opts = {}
+    def initialize(app, opts = {})
       @app = app
       @project_id = opts[:project_id]
     end
 
-    def call env
+    def call(env)
+      replace_logger
       original_middeware.call(env)
     end
 
@@ -16,6 +17,14 @@ module GoogleCloudLoggingOnFork
 
       require 'google/cloud/logging/middleware'
       @original_middeware = Google::Cloud::Logging::Middleware.new(@app)
+    end
+
+    def replace_logger
+      original_logger = Rails.logger
+      if original_logger.is_a?(::ActiveSupport::Logger)
+        Rails.logger = original_middeware.logger
+        Rails.logger.extend(ActiveSupport::Logger.broadcast(original_logger))
+      end
     end
 
     def project_id
