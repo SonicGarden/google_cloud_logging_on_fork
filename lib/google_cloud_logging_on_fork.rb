ce51492950f5fc352f86e3f51156498d13c9ff9b
Akihiro MATSUMURA <matsumura.aki@gmail.com>
2017-12-18 20:20:53 +0900

    add config for keyfile

--- lib/google_cloud_logging_on_fork.rb
+++ lib/google_cloud_logging_on_fork.rb
@@ -4,12 +4,17 @@ module GoogleCloudLoggingOnFork
   def self.load(project_id = nil)
     require 'google/cloud/logging'
     Google::Cloud::Logging.configure do |config|
-      config.project_id = project_id || rails_conf_project_id
+      config.project_id = project_id || rails_config_project_id
+      config.keyfile = rails_config_keyfile
     end
   end
 
-  def self.rails_conf_project_id
+  def self.rails_config_project_id
     defined?(::Rails) && ::Rails.application.config.google_cloud_logging_on_fork.project_id
+  end
+
+  def self.rails_config_keyfile
+    defined?(::Rails) && ::Rails.application.config.google_cloud_logging_on_fork.keyfile
   end
 end
 
