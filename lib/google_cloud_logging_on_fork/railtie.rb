c7e46cb5b60341bb1e7b42b8d28a25a55a518946
Akihiro MATSUMURA <matsumura.aki@gmail.com>
2017-12-19 15:53:22 +0900

    設定がないときは、起動しないように

--- lib/google_cloud_logging_on_fork/railtie.rb
+++ lib/google_cloud_logging_on_fork/railtie.rb
@@ -8,8 +8,12 @@ module GoogleCloudLoggingOnFork
     config.google_cloud_logging_on_fork.logging.monitored_resource.labels = ::ActiveSupport::OrderedOptions.new
 
     initializer "GoogleCloudLoggingOnFork", before: :initialize_logger do |app|
-      app.middleware.insert_before Rails::Rack::Logger, GoogleCloudLoggingOnFork::Middleware
-      self.class.spring_after_fork
+      if ::Rails.application.config.google_cloud_logging_on_fork.project_id
+        app.middleware.insert_before Rails::Rack::Logger, GoogleCloudLoggingOnFork::Middleware
+        self.class.spring_after_fork
+      else
+        puts "[GoogleCloudLoggingOnFork] Not initialized, project_id is blank."
+      end
     end
 
     def self.spring_after_fork
